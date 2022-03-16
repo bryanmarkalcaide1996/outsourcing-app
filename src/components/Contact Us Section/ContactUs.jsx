2c39ce0f4fee1cbab247b9ee6e8c2cdf9ef25f64
Mackinley de la Cruz <rily.rnmd@gmail.com>
2022-03-16 21:52:26 +0800

    7th

--- src/components/Contact Us Section/ContactUs.jsx
+++ src/components/Contact Us Section/ContactUs.jsx
@@ -2,7 +2,6 @@ import './ContactUs.css';
 import React, {useState, useRef, useEffect} from 'react'
 
 const ContactUs = () => {
-  const [id, setId] = useState(Date.now())
     const fNameRef = useRef(null);
     const emailRef = useRef(null);
     const subjectRef = useRef(null);
@@ -19,15 +18,17 @@ const ContactUs = () => {
         }
   }
   return (
-    <div>
+    <div className='container'>
+      <div className='bg-card'>
         <form className='form' action="" onSubmit={handleSubmit}>
            <h1>Contact Us</h1> 
             <input type="text" placeholder='Full Name' ref={fNameRef}/>
             <input type="email" placeholder='Email' ref={emailRef}/>
             <input type="text" placeholder='Subject' ref={subjectRef}/>
             <textarea name="" id="" cols="48" rows="10" placeholder='Your message' ref={messageRef}/>
-            <input type="submit" />
+            <input type="submit" value='Send Message'/>
         </form>
+    </div>
     </div>
   )
 }
